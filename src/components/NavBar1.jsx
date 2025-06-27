b4e9d0c02d4543dde5f619c25d874e7d8916715e
KF1708 <kftech.va@gmail.com>
2025-06-27 18:55:31 -0400

    fix

--- src/components/NavBar1.jsx
+++ src/components/NavBar1.jsx
@@ -1,11 +1,12 @@
 import { Input } from "antd";
 import { ProfileIcon, SearchIcon, ShoppingCart } from "./Icon";
+import logo from "../public/icons/logo_white 1.svg";
 
 const NavBar1 = () => {
   return (
     <div className="navbar-upper-section gap-5 py-3">
       <div className="navbar_logo">
-        <img src="/icons/logo_white 1.svg" alt="logo" />
+        <img src={logo} alt="logo" />
       </div>
       <div className="navbar-upper-section gap-3">
         <div className="search-input">
